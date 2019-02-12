b6f47476e10814cd913d46a89fbcd6fa9adb3459
broberts19 <brianamarieroberts@gmail.com>
2019-02-12 06:39:57 +0000

    Added in Gruntfile for realtime css changes, replaced images for Skill Set, and made some html changes.

---
+++ Gruntfile.js
@@ -0,0 +1,104 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'sass',
+                    cssDir: 'css',
+                    environment: 'production'
+                }
+            },
+            dev: {
+                options: {
+                    sassDir: 'sass',
+                    cssDir: 'css'
+                }
+            }
+        },
+
+        //looks like the agency.scss file is used to create the other agency.css files
+        //look into where I got this file from
+
+        watch: {
+            css:{
+                files: ['css/agency.css', 'scss/*.scss'],
+                tasks: ['cssmin'],
+                options: {
+                    livereload: true
+                }
+            },
+            js:{
+                files: ['js/*.js'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';',
+                stripBanners: true,
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+
+            dist: {
+                src: ['js/*.js'],
+                dest: 'js/main.min.js'
+            }
+        },
+
+
+        uglify:{
+            options: {
+                manage: false,
+                preserveComments: 'all' //preserve all comments on JS files
+            },
+            my_target:{
+                files: {
+                    'js/main.min.js' : ['js/*.js']
+                }
+            }
+        },
+
+
+        cssmin:{
+            my_target:{
+                files: [{
+                    expand: true,
+                    cwd: 'css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css/',
+                    ext: '.min.css'
+
+                }]
+            }
+        }
+
+    });
+
+    // Load the plugin that provides the "compass" task.
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // Load the plugin that provides the "watch" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Load the plugin that provides the "sass" task.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Load the plugin that provides the "concat" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Load the plugin that provides the "cssmin" task.
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify','cssmin']);
+};
